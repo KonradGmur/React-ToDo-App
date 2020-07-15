916fc1da95574a50da2768bb5d0171cf204af449
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-15 19:02:05 +0200

    Task addition form

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
 import './AddTask.css'
 class AddTask extends Component {
-    state = {
-        text: "",
-        checked: false,
 
+    minDate = new Date().toISOString().slice(0, 10);
+    state = {
+        text: '',
+        checked: false,
+        date: this.minDate,
     }
+
+    handleDate = (e) => {
+        this.setState({
+            date: e.target.value
+        })
+    }
+
     render() {
+        let maxDate = this.minDate.slice(0, 4) * 1 + 1;
+        console.log(maxDate);
+        maxDate = maxDate + "-12-31"
+
+
         return (
             <div className="form">
                 <input type="text" placeholder="Dodaj zadanie" value={this.state.text} />
                 <input type="checkbox" checked={this.state.checked} id="important" />
                 <label htmlFor="important">Priorytet</label><br />
                 <label htmlFor="date">Do kiedy zrobić: </label><br />
-                <input type="date" value='2020-07-15' min='2020-07-15' max='2020-12-31' /><br/>
+                <input type="date" value={this.state.date} min={this.minDate} max={maxDate} onChange={this.handleDate} /><br />
                 <button>Add</button>
             </div >
         );
