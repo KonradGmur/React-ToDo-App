3692c1833a7528a100d19cf2505ce8707992b41e
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-15 15:23:52 +0200

    Adds the initial application

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  state = {}
+
+  render() {
+    return (
+      <div>
+        Aplikacja TODO
+      </div>
+    );
+  }
+}
+
+export default App;
+
