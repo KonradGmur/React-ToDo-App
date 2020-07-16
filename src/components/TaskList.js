af44753fd115166300a0490ad9e63f2251743834
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-16 12:14:43 +0200

    Adding the sorting function to the tasks done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -7,7 +7,19 @@ const TaskList = (props) => {
     const done = props.tasks.filter(task => !task.active)
 
     done.sort((a, b) => b.finishDate - a.finishDate)
-   
+
+    if (active.length >= 2) {
+        active.sort((a, b) => {
+
+            a = a.text.toLowerCase();
+            b = b.text.toLowerCase();
+
+            if (a.text < b.text) return -1;
+            if (a.text > b.text) return -1;
+            return 0
+        })
+    }
+
     const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
     const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
 
