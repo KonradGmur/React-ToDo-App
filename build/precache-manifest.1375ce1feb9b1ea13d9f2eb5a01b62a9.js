233cc80d422830027e7c92e0a1d7a62b253f7f5e
Konrad Gmur <konrad.gmur@gmail.com>
2020-07-16 13:00:12 +0200

    Update all aplication

---
+++ build/precache-manifest.1375ce1feb9b1ea13d9f2eb5a01b62a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "87687b5476e1bc5c4351",
+    "url": "/static/css/main.1fc59a69.chunk.css"
+  },
+  {
+    "revision": "87687b5476e1bc5c4351",
+    "url": "/static/js/main.87687b54.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ba902f0cdd66954519e13583ab6a857",
+    "url": "/index.html"
+  }
+];
